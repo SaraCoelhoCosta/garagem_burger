abda1e707e130cb16e50da182d2e3f77d7ef3f07
WilliamJSS <williamsena088@gmail.com>
2022-04-24 20:41:23 -0300

    Atualização do card de pedido

--- lib/screens/components/card_pedido.dart
+++ lib/screens/components/card_pedido.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CardPedido extends StatelessWidget {
+  
   final String text;
   final IconData icon;
-  final MaterialColor color;
+  final Color color;
 
   const CardPedido({
     Key? key,
@@ -18,32 +19,43 @@ class CardPedido extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          side: BorderSide(
+            width: 2.0,
+            color: color,
+          ),
+        ),
         elevation: 6.0,
-        color: color,
         child: ListTile(
           leading: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+            borderRadius: BorderRadius.circular(15.0),
             child: Image.asset('images/hamburguer.jpg'),
           ),
           title: Text(
             text,
             style: GoogleFonts.oxygen(
-              fontSize: 20.0,
+              fontSize: 16.0,
               fontWeight: FontWeight.bold,
             ),
           ),
           subtitle: Text(
             'às 21:00',
             style: GoogleFonts.oxygen(
-              fontSize: 20.0,
+              fontSize: 16.0,
             ),
           ),
-          contentPadding: const EdgeInsets.all(6),
+          contentPadding: const EdgeInsets.only(
+            bottom: 6,
+            left: 15,
+            right: 6,
+            top: 6,
+          ),
           trailing: Padding(
             padding: const EdgeInsets.only(top: 37, right: 5),
             child: Icon(
               icon,
-              color: Colors.black,
+              color: Colors.grey[700],
             ),
           ),
         ),
--- lib/screens/telas_perfil/tela_meus_pedidos.dart
+++ lib/screens/telas_perfil/tela_meus_pedidos.dart
@@ -8,6 +8,109 @@ class TelaMeusPedidos extends StatelessWidget {
   const TelaMeusPedidos({Key? key, this.telaVazia = false}) : super(key: key);
 
   Widget telaMeusPedidosVazia() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ListView(
+          shrinkWrap: true,
+          children: [
+            const Icon(Icons.content_paste, size: 100),
+            const SizedBox(height: 15),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 300,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'IR PARA O MENU',
+                      style: GoogleFonts.oxygen(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 80, bottom: 70, left: 50, right: 50),
+                  child: Text(
+                    'Você ainda não fez nenhum pedido.',
+                    style: GoogleFonts.oxygen(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'Encontre o produto que deseja no Menu.',
+              style: GoogleFonts.oxygen(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20)
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget telaMeusPedidos() {
+    return ListView(
+      children: [
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              label: Text('Apagar histórico de pedidos',
+                  style: GoogleFonts.oxygen(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                  textAlign: TextAlign.left),
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        const CardPedido(
+          text: 'Pedido pendente em 24/04/2022',
+          icon: Icons.error_outline,
+          color: Color(0xfffed80b),
+        ),
+        const CardPedido(
+          text: 'Pedido entregue em 25/04/2022',
+          icon: Icons.check_circle_outline,
+          color: Colors.green,
+        ),
+        const CardPedido(
+          text: 'Pedido cancelado em 23/04/2022',
+          icon: Icons.cancel_outlined,
+          color: Colors.red,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xfffed80b),
@@ -20,124 +123,7 @@ class TelaMeusPedidos extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          ListView(
-            shrinkWrap: true,
-            children: [
-              const Icon(Icons.content_paste, size: 100),
-              const SizedBox(height: 15),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  SizedBox(
-                    height: 100,
-                    width: 300,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'IR PARA O MENU',
-                        style: GoogleFonts.oxygen(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 80, bottom: 70, left: 50, right: 50),
-                    child: Text(
-                      'Você ainda não fez nenhum pedido.',
-                      style: GoogleFonts.oxygen(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                'Encontre o produto que deseja no Menu.',
-                style: GoogleFonts.oxygen(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20)
-            ],
-          ),
-        ],
-      ),
+      body: (telaVazia) ? telaMeusPedidosVazia() : telaMeusPedidos(),
     );
-  }
-
-  Widget telaMeusPedidos() {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xfffed80b),
-        foregroundColor: Colors.black,
-        centerTitle: true,
-        title: Text(
-          'Meus pedidos',
-          style: GoogleFonts.keaniaOne(
-            fontSize: 26.0,
-          ),
-        ),
-      ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton.icon(
-                onPressed: () {},
-                label: Text('Apagar histórico de pedidos',
-                    style: GoogleFonts.oxygen(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                    ),
-                    textAlign: TextAlign.left),
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          const CardPedido(
-            text: 'Pedido pendente em 24/04/2022',
-            icon: Icons.error_outline,
-            color: Colors.yellow,
-          ),
-          const CardPedido(
-            text: 'Pedido entregue em 25/04/2022',
-            icon: Icons.check_circle_outline,
-            color: Colors.green,
-          ),
-          const CardPedido(
-            text: 'Pedido cancelado em 23/04/2022',
-            icon: Icons.cancel_outlined,
-            color: Colors.red,
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: (telaVazia) ? telaMeusPedidosVazia() : telaMeusPedidos());
   }
 }
