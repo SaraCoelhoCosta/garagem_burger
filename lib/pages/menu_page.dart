0a7282ff94b1350bdccbe003c3f20e841fde4924
WilliamJSS <williamsena088@gmail.com>
2022-06-10 13:47:55 -0300

    Atualização no design dos filtros

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -5,7 +5,6 @@ import 'package:garagem_burger/controllers/products.dart';
 import 'package:provider/provider.dart';
 
 enum Filter {
-  all,
   montarHamburguer,
   ofertas,
   hamburgueres,
@@ -26,7 +25,16 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
-  Filter filter = Filter.all;
+  bool showFilters = false;
+  int? currentFilter;
+  List<String> filters = [
+    'Montar Hambúrguer',
+    'Hambúrgueres da Casa',
+    'Combos',
+    'Acompanhamentos',
+    'Sobremesas',
+    'Bebidas',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +54,12 @@ class _MenuPageState extends State<MenuPage> {
               /*
               * Botão de filtrar
               */
-              PopupMenuButton(
+              GestureDetector(
+                onTap: () {
+                  if (currentFilter == null) {
+                    setState(() => showFilters = !showFilters);
+                  }
+                },
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: const [
@@ -60,41 +73,56 @@ class _MenuPageState extends State<MenuPage> {
                     ),
                   ],
                 ),
-                initialValue: filter,
-                onSelected: (selectedFilter) {
-                  setState(() => filter = selectedFilter as Filter);
-                },
-                itemBuilder: (ctx) => [
-                  const PopupMenuItem(
-                    value: Filter.all,
-                    child: CustomText('Tudo'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.montarHamburguer,
-                    child: CustomText('Montar Hambúrguer'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.hamburgueres,
-                    child: CustomText('Hambúrgueres da Casa'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.acompanhamentos,
-                    child: CustomText('Acompanhamentos'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.sobremesas,
-                    child: CustomText('Sobremesas'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.bebidas,
-                    child: CustomText('Bebidas'),
-                  ),
-                  const PopupMenuItem(
-                    value: Filter.combos,
-                    child: CustomText('Combos'),
-                  ),
-                ],
               ),
+              // PopupMenuButton(
+              //   child: Row(
+              //     mainAxisSize: MainAxisSize.min,
+              //     children: const [
+              //       Icon(
+              //         Icons.filter_list,
+              //         size: 30,
+              //       ),
+              //       CustomText(
+              //         '  Filtros',
+              //         fontWeight: FontWeight.bold,
+              //       ),
+              //     ],
+              //   ),
+              //   initialValue: filter,
+              //   onSelected: (selectedFilter) {
+              //     setState(() => filter = selectedFilter as Filter);
+              //   },
+              //   itemBuilder: (ctx) => [
+              //     const PopupMenuItem(
+              //       value: Filter.all,
+              //       child: CustomText('Tudo'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.montarHamburguer,
+              //       child: CustomText('Montar Hambúrguer'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.hamburgueres,
+              //       child: CustomText('Hambúrgueres da Casa'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.acompanhamentos,
+              //       child: CustomText('Acompanhamentos'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.sobremesas,
+              //       child: CustomText('Sobremesas'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.bebidas,
+              //       child: CustomText('Bebidas'),
+              //     ),
+              //     const PopupMenuItem(
+              //       value: Filter.combos,
+              //       child: CustomText('Combos'),
+              //     ),
+              //   ],
+              // ),
               /*
               * Perfil de usuário
               */
@@ -113,9 +141,46 @@ class _MenuPageState extends State<MenuPage> {
           ),
         ),
         /*
+        * Filtros
+        */
+        if (showFilters)
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            height: 50,
+            child: ListView.builder(
+              itemCount: filters.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (ctx, index) {
+                return Offstage(
+                  offstage:
+                      (currentFilter == null) ? false : currentFilter != index,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (currentFilter == index) {
+                          setState(() => currentFilter = null);
+                        } else {
+                          setState(() => currentFilter = index);
+                        }
+                      },
+                      child: Chip(
+                        label: CustomText(filters[index]),
+                        backgroundColor: (currentFilter == index)
+                            ? Theme.of(context).backgroundColor
+                            : null,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        /*
         * Botão Monte seu Próprio Hamburguer
         */
-        if (filter == Filter.all || filter == Filter.montarHamburguer)
+        if (currentFilter == null || currentFilter == 0)
           GestureDetector(
             onTap: () {},
             child: Container(
@@ -123,7 +188,7 @@ class _MenuPageState extends State<MenuPage> {
                 minHeight: 200,
                 maxHeight: 346,
               ),
-              margin: const EdgeInsets.symmetric(horizontal: 10),
+              margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.all(
                   Radius.circular(15.0),
@@ -150,27 +215,27 @@ class _MenuPageState extends State<MenuPage> {
         /*
         * Listas de produtos
         */
-        if (filter == Filter.all || filter == Filter.hamburgueres)
+        if (currentFilter == null || currentFilter == 1)
           ProductList(
             title: 'HAMBÚRGUERES DA CASA',
             produtos: provider.hamburgueres,
           ),
-        if (filter == Filter.all || filter == Filter.combos)
+        if (currentFilter == null || currentFilter == 2)
           ProductList(
             title: 'COMBOS',
             produtos: provider.combos,
           ),
-        if (filter == Filter.all || filter == Filter.acompanhamentos)
+        if (currentFilter == null || currentFilter == 3)
           ProductList(
             title: 'ACOMPANHAMENTOS',
             produtos: provider.acompanhamentos,
           ),
-        if (filter == Filter.all || filter == Filter.sobremesas)
+        if (currentFilter == null || currentFilter == 4)
           ProductList(
             title: 'SOBREMESAS',
             produtos: provider.sobremesas,
           ),
-        if (filter == Filter.all || filter == Filter.bebidas)
+        if (currentFilter == null || currentFilter == 5)
           ProductList(
             title: 'BEBIDAS',
             produtos: provider.bebidas,
